36ca970d5ea670c7a23594e6628cba70947f987a
monzag <zaganiacz.m@gmail.com>
2018-02-07 20:20:21 +0100

    Add quicksort method

--- src/main/java/com/codecool/quicksort/QuickSort.java
+++ src/main/java/com/codecool/quicksort/QuickSort.java
@@ -17,4 +17,30 @@ public class QuickSort {
         quicksort(toSort, low, high);
     }
 
+    private void quicksort(List<Integer> toSort, int low, int high) {
+        Integer pivot = toSort.get(random.nextInt(toSort.size()));
+        int i = low;
+        int j = high;
+
+        while (i <= j) {
+            while (toSort.get(i) < pivot) {
+                i++;
+            }
+            while (toSort.get(j) > pivot) {
+                j--;
+            }
+            if (i <= j) {
+                exchange(toSort, i, j);
+                i++;
+                j--;
+            }
+        }
+
+        if (low < j) {
+            quicksort(toSort, low, j);
+        } if (i < high) {
+            quicksort(toSort, i, high);
+        }
+    }
+
 }
